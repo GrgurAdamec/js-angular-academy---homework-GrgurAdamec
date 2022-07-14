b21d98c4067ebc0c898877cda537f99341913186
GrgurAdamec <adamec.grgur@gmail.com>
2022-07-14 10:39:48 +0200

    Added top rated shows component

---
+++ src/app/pages/top-rated-shows/top-rated-shows.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-top-rated-shows',
+	templateUrl: './top-rated-shows.component.html',
+	styleUrls: ['./top-rated-shows.component.scss'],
+})
+export class TopRatedShowsComponent {}
---
+++ src/app/pages/top-rated-shows/top-rated-shows.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TopRatedShowsComponent } from './top-rated-shows.component';
+
+@NgModule({
+	declarations: [TopRatedShowsComponent],
+	imports: [CommonModule],
+})
+export class TopRatedShowsModule {}

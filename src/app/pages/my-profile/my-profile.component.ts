e7b2ecc882a82a62376125e1014ba72ac9fec6c6
Grgur Adamec <adamec.grgur@gmail.com>
2022-07-28 07:50:31 +0200

    css and my profile page

---
+++ src/app/pages/my-profile/my-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { ReviewService } from 'app/services/show-services/review.service';
+
+@Component({
+	selector: 'app-my-profile',
+	templateUrl: './my-profile.component.html',
+	styleUrls: ['./my-profile.component.scss'],
+})
+export class MyProfileComponent {
+	constructor(public readonly reviewService: ReviewService) {}
+}
